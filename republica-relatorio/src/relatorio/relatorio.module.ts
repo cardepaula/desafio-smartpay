dc16ee6b83031c190570f231680e3986e66055f8
Marcos A C de Paula <cardepaula@gmail.com>
2020-01-02 21:22:43 -0300

    refactor(typeorm): Import TypeOrmModule on RelatorioModule

--- republica-relatorio/src/relatorio/relatorio.module.ts
+++ republica-relatorio/src/relatorio/relatorio.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { RelatorioController } from './controllers/relatorio.controller';
 import { RelatorioService } from './services/relatorio.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { estabelecimentoEntity, produtoEntity } from './models/entities';
 
 @Module({
+  imports: [TypeOrmModule.forRoot({
+    name: "default",
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "pass",
+    database: "postgres",
+    schema: "public",
+    synchronize: false,
+    entities: [
+      estabelecimentoEntity,
+      produtoEntity
+    ]
+  })],
   controllers: [RelatorioController],
   providers: [RelatorioService]
 })
