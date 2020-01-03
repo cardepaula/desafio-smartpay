0fcd49a062f6d3a76c009a9efcf88268a049cd12
Marcos A C de Paula <cardepaula@gmail.com>
2020-01-02 22:06:29 -0300

    feat(dotenv): Add dotenv

--- republica-relatorio/src/relatorio/relatorio.module.ts
+++ republica-relatorio/src/relatorio/relatorio.module.ts
@@ -8,12 +8,12 @@ import { estabelecimentoEntity, produtoEntity } from './models/entities';
   imports: [TypeOrmModule.forRoot({
     name: "default",
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "pass",
-    database: "postgres",
-    schema: "public",
+    host: process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB__PASS || "pass",
+    database: process.env.DB_DATABASE || "postgres",
+    schema: process.env.DB_SCHEMA || "public",
     synchronize: false,
     entities: [
       estabelecimentoEntity,
