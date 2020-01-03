f7826992b1c387e40f73b9b2a0a01258d3dc222e
Marcos A C de Paula <cardepaula@gmail.com>
2020-01-03 16:49:16 -0300

    refactor(swagger): Add swagger in main.ts

--- republica-relatorio/src/main.ts
+++ republica-relatorio/src/main.ts
@@ -2,9 +2,29 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
+
+const pacote = require('../package.json')
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle( 'Republica S/A' )
+    .setDescription( pacote.description )
+    .setVersion( pacote.version )
+    .addTag( 'relatorio' )
+    .addTag( 'health-check' )
+    .build();
+  
+  const document = SwaggerModule.createDocument( app, options );
+
+  // para gerar o swagger.json (copiando a saida do console)
+  fs.writeFileSync( 'swagger.json', JSON.stringify( document ) );
+
+  SwaggerModule.setup( 'docs', app, document );
+
   await app.listen(3000);
 }
 bootstrap();
