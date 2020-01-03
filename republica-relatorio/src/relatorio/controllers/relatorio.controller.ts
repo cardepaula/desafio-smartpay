44b54830a60959a21fd2952cf31bdf9e33109710
Marcos A C de Paula <cardepaula@gmail.com>
2020-01-02 21:10:19 -0300

    refactor(relatorio): Refactor controller and service

--- republica-relatorio/src/relatorio/controllers/relatorio.controller.ts
+++ republica-relatorio/src/relatorio/controllers/relatorio.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Res, HttpStatus, HttpException } from '@nestjs/common';
+import { Response } from 'express';
 import { RelatorioService } from '../services/relatorio.service';
 
 @Controller('relatorio')
@@ -11,20 +12,33 @@ export class RelatorioController {
     }
 
     @Get('produtos/:cnpj')
-    async getProdutos() {
-        const produtos = await this.relatorioService.getProdutos(); 
-        return produtos;
+    async getProdutosDeEstabelecimento(@Res() res: Response, @Param() params: {cnpj: string}) {
+        try {
+            const produtos = await this.relatorioService.getProdutosDeEstabelecimento(parseInt(params.cnpj, 10)); 
+            res.status( HttpStatus.OK ).send( produtos );
+        } catch (error) {
+            throw new HttpException(error, HttpStatus.BAD_REQUEST);   
+        }
     }
 
-    @Get('produto-estabelecimento/:categoria')
-    async getCategorias() {
-        var a = await this.relatorioService.getCategorias(); 
-        return a;
+    @Get('produtos-categoria/:categoria')
+    async getProdutosDeCategoria(@Res() res: Response, @Param() params: {categoria: string}) {
+        try {
+            var produtosPorCategoria = await this.relatorioService.getProdutosDeCategoria(params.categoria.toLowerCase()); 
+            res.status( HttpStatus.OK ).send( produtosPorCategoria );
+        } catch (error) {
+            throw new HttpException(error, HttpStatus.BAD_REQUEST);
+        }
     }
     
     @Get('estabelecimentos/:produto')
-    async getEstabelecimentos() {
-        var estabelecimentos = await this.relatorioService.getEstabelecimentos(); 
-        return estabelecimentos;
+    async getEstabelecimentosDeProduto(@Res() res: Response, @Param() params: {produto: string}) {
+
+        try {
+            var estabelecimentos = await this.relatorioService.getEstabelecimentosDeProduto(params.produto.toLowerCase()); 
+            res.status( HttpStatus.OK ).send( estabelecimentos );
+        } catch (error) {
+            throw new HttpException(error, HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- republica-relatorio/src/relatorio/services/relatorio.service.ts
+++ republica-relatorio/src/relatorio/services/relatorio.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { produtoEntity, estabelecimentoEntity } from '../models/entities';
+import { EstabelecimentoDeProdutoDto, ProdutoDeCategoriaDto, ProdutoDeEstabelecimentoDto } from '../models/dto';
 
 @Injectable()
 export class RelatorioService {
@@ -6,15 +8,65 @@ export class RelatorioService {
     return 'Relatorio';
   }
 
-  async getProdutos() {
+  async getProdutosDeEstabelecimento(cnpjParam: number): Promise<ProdutoDeEstabelecimentoDto[]> {
       
+    let produtosDto: ProdutoDeEstabelecimentoDto[] = [];
+    const estabelecimento = await estabelecimentoEntity.findOne({where: {cnpj: cnpjParam}, relations: ['produtos']});
+
+    produtosDto = estabelecimento.produtos.map( produtos => {
+      return new ProdutoDeEstabelecimentoDto(produtos);
+    }).sort(this.compareProds);
+
+    return produtosDto;
   }
 
-  async getCategorias() {
-      
+  async getProdutosDeCategoria(categoriaParam: string): Promise<ProdutoDeCategoriaDto[]> {
+
+    let produtosPorCategoria: ProdutoDeCategoriaDto[] = [];  
+    const produtos = await produtoEntity.find({where: {categoria: categoriaParam}, relations: ['estabelecimentos']});
+
+    produtosPorCategoria = produtos.map( produto => {
+      return new ProdutoDeCategoriaDto(produto);
+    }).sort(this.compareProdCateg);
+
+    return produtosPorCategoria;
   }
   
-  async getEstabelecimentos() {
+  async getEstabelecimentosDeProduto(produtoParam: string): Promise<EstabelecimentoDeProdutoDto[]> {
 
+    let estabelecimentoPorProduto: EstabelecimentoDeProdutoDto[] = [];
+    const produto = await produtoEntity.findOne({where: {name: produtoParam}, relations: ['estabelecimentos']});
+
+    estabelecimentoPorProduto = (await produto.estabelecimentos).map( estabelecimento => {
+      return new EstabelecimentoDeProdutoDto(estabelecimento);
+    })
+
+    return estabelecimentoPorProduto;
+  }
+
+  private compareProdCateg(a: ProdutoDeCategoriaDto, b: ProdutoDeCategoriaDto) {
+    const produtoA = a.produto;
+    const produtoB = b.produto;
+  
+    let comparison = 0;
+    if (produtoA > produtoB) {
+      comparison = 1;
+    } else if (produtoA < produtoB) {
+      comparison = -1;
+    }
+    return comparison;
+  }
+
+  private compareProds(a: ProdutoDeEstabelecimentoDto, b: ProdutoDeEstabelecimentoDto) {
+    const produtoA = a.nome;
+    const produtoB = b.nome;
+  
+    let comparison = 0;
+    if (produtoA > produtoB) {
+      comparison = 1;
+    } else if (produtoA < produtoB) {
+      comparison = -1;
+    }
+    return comparison;
   }
 }
